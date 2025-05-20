9261ed38f902d7c496267f44a33b5ad55c1e3aec
Jaime Romero <jaimerzuleta@gmail.com>
2025-05-20 16:39:53 +0200

    adding src files

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-100">
+      {/* App is now managed through the router in main.tsx */}
+    </div>
+  );
+}
+
+export default App;
