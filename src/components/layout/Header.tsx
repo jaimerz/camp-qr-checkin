4e3558e0e797e2964730200c5812a9fac3a0a518
jaimerz <jaimerzuleta@gmail.com>
2025-05-23 00:14:29 +0200

    add missing import

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { getEvents } from '../../utils/firebase';
 import { Event } from '../../types';
 import { Link, useNavigate } from 'react-router-dom';
