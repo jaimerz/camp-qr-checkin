714f27738560b823797110d74880a2e59b1a0416
jaimerz <jaimerzuleta@gmail.com>
2025-05-23 12:14:26 +0200

    fix typo

--- src/pages/ManageParticipants.tsx
+++ src/pages/ManageParticipants.tsx
@@ -1,4 +1,4 @@
-""import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { Download, RefreshCw, Users, PlusCircle, Trash2 } from 'lucide-react';
 import AuthGuard from '../components/AuthGuard';
