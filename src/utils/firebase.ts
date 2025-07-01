e959ff6198f6490ddb54e4aeb3bd50142c7d304c
Jaime Romero <jaimerzuleta@gmail.com>
2025-07-01 12:59:04 +0200

    Update firebase.ts

--- src/utils/firebase.ts
+++ src/utils/firebase.ts
@@ -18,12 +18,12 @@ import { getFirestore,
 import { User, UserRole, Participant, Activity, ActivityLog, Event } from '../types';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAKaqOCzuU6si-EIKxcySZwbYR2stozSPc",
-  authDomain: "camp-qr-checkin.firebaseapp.com",
-  projectId: "camp-qr-checkin",
-  storageBucket: "camp-qr-checkin.firebasestorage.app",
-  messagingSenderId: "17791451766",
-  appId: "1:17791451766:web:98a01f90888217f6e29557"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
