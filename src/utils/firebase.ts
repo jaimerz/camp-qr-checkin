c61c29cffa7e7dde833841fa7580da0acc63cdeb
Jaime Romero <jaimerzuleta@gmail.com>
2025-07-02 23:09:38 +0200

    export auth

--- src/utils/firebase.ts
+++ src/utils/firebase.ts
@@ -28,7 +28,7 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
+export const auth = getAuth(app);
 export const db = getFirestore(app);
 
 // Participant related functions
